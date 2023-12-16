15aae0bf10a26838b45edc0040241eb938f406ce
nim0y <nim0y33@gmail.com>
2023-12-16 18:19:48 +0300

    Sprint #18 #2

--- app/src/main/java/com/example/playlistmaker/ui/mediateka/FavoriteTracksFragment.kt
+++ app/src/main/java/com/example/playlistmaker/ui/mediateka/FavoriteTracksFragment.kt
@@ -6,11 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.playlistmaker.databinding.FragmentFavoriteTracksBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class FavoriteTracksFragment : Fragment() {
 
     private var _binding: FragmentFavoriteTracksBinding? = null
     private val binding get() = _binding!!
+
+    private val vm by viewModel<FavoriteTracksViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,8 +21,12 @@ class FavoriteTracksFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentFavoriteTracksBinding.inflate(layoutInflater, container, false)
-
         return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
